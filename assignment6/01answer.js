86bd95c97191cd54961141b2117a139d87c881be
Thomas Hill <60381497+hillt5@users.noreply.github.com>
2021-11-18 04:37:03 -0500

    Added files for assignment 6
    Several changes to existing code to make sure preview.html loads all scripts. Also, changed source of .csv to github raw file. Will update this to my own file later

---
+++ assignment6/01answer.js
@@ -0,0 +1,22 @@
+d3.csv('https://raw.githubusercontent.com/charleyferrari/CUNY_DATA_608/master/module6/d3_lab/ue_industry.csv', data => {
+
+    const xScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => +d.index))
+        .range([20, 1180]);
+            
+    const yScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => +d.Agriculture))
+        .range([580, 20]);
+
+    var line3 = d3.line()
+        .x(d => xScale(d.index))
+        .y(d => yScale(d.Agriculture));
+
+        
+    d3.select('#answer1')
+        .append('path')
+        .attr('d', line3(data))
+        .attr('stroke', '#2e2928');
+
+
+});
